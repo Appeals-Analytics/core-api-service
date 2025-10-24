9867840752fa43f81510d49a552cc64409103d75
kuyltep <vladpetlu91@gmail.com>
2025-10-24 22:22:56 +0300

    add lifespan for kafka

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 
 from src.config import configs
 from src.api import main_router
+from src.utils import lifespan
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(main_router)
+
 
 if __name__ == "__main__":
   uvicorn.run("src.main:app", host="0.0.0.0", port=configs.app_port)
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from src.services.kafka.kafka_service import kafka_service
+from src.services.kafka.config import kafka_settings
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  
+  await kafka_service.start_producer()
+  await kafka_service.start_consumer([kafka_settings.topic_in])
+  
+  yield
+  
+  await kafka_service.close()
