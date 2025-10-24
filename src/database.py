b2eec5dff89bf6ef69c08458262337f7172def99
kuyltep <vladpetlu91@gmail.com>
2025-10-24 21:10:17 +0300

    add exceptions and database

---
+++ src/database.py
@@ -0,0 +1,1 @@
+#logic for connect to database
---
+++ src/exceptions.py
@@ -0,0 +1,15 @@
+from fastapi import HTTPException, status
+def BAD_REQUEST_EXCEPTION(detail: str) -> HTTPException:
+  return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+def INTERNAL_SERVER_ERROR_EXCEPTION(detail: str) -> HTTPException:
+  return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
+
+def FORRBIDEN_ERROR_EXCEPTION(detail: str) -> HTTPException:
+  return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
+
+def UNAUTHORIZED_EXCEPTION(detail: str) -> HTTPException:
+  return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
+
+def CONTENT_TOO_LARGE_EXCEPTION(detail: str) -> HTTPException:
+  return HTTPException(status_code=status.HTTP_413_CONTENT_TOO_LARGE, detail=detail)
