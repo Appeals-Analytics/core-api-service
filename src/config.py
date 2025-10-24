06efb47d9dc3aa21f7854cd24cd3f79ce1694c34
kuyltep <vladpetlu91@gmail.com>
2025-10-24 22:22:08 +0300

    update base configs

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Configs(BaseSettings):
   app_port: int
   database_url: SecretStr
   
-  model_config = SettingsConfigDict(env_file=".env")
+  model_config = SettingsConfigDict(env_file=".env", extra="ignore")
   
   
 configs = Configs()
