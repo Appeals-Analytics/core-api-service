21823017183e80d308a034adcb1f4fd8a26bcb7b
kuyltep <vladpetlu91@gmail.com>
2025-10-27 19:50:02 +0300

    add database configuration

--- src/config.py
+++ src/config.py
@@ -4,7 +4,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Configs(BaseSettings):
   
   app_port: int
-  database_url: SecretStr
+  
+  db_user: SecretStr
+  db_password: SecretStr
+  
+  db_host: SecretStr
+  db_port: int
+  db_name: SecretStr
+  
+  def get_db_url(self):
+    return (f"postgresql+asyncpg://{self.db_user.get_secret_value()}:{self.db_password.get_secret_value()}@"
+            f"{self.db_host.get_secret_value()}:{self.db_port}/{self.db_name.get_secret_value()}")
   
   model_config = SettingsConfigDict(env_file=".env", extra="ignore")
   
--- src/database.py
+++ src/database.py
@@ -1,1 +1,13 @@
-#logic for connect to database
+from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from src.config import configs
+
+DATABASE_URL = configs.get_db_url()
+
+engine = create_async_engine(url=DATABASE_URL)
+
+async_session = sessionmaker(engine, class_=AsyncConnection, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+  pass
