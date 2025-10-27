ddc9f81683bb93f5294fa60c0931ee8b315996e4
kuyltep <vladpetlu91@gmail.com>
2025-10-27 20:47:29 +0300

    update utils and exceptions

---
+++ src/api/batch_data/exceptions.py
@@ -0,0 +1,3 @@
+from src.exceptions import CONTENT_TOO_LARGE_EXCEPTION
+
+BATCH_TOO_LARGE_TO_PROCESSING = CONTENT_TOO_LARGE_EXCEPTION("Batch is to large to processing")
--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,8 @@ async def lifespan(app: FastAPI):
   await kafka_service.start_producer()
   await kafka_service.start_consumer([kafka_settings.topic_in])
   
+  print("Kafka connect succesfully")
+  
   yield
   
   await kafka_service.close()
