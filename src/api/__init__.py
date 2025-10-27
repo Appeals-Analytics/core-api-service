35e90da9f55f204cf1aaf6a62f0784e2a7ba42e0
kuyltep <vladpetlu91@gmail.com>
2025-10-27 20:47:45 +0300

    update router for batch processing

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.api.files.router import files_router
+from src.api.batch_data.router import batch_router
 
 main_router = APIRouter()
 
 main_router.include_router(files_router)
+main_router.include_router(batch_router)
