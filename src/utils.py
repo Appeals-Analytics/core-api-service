f97c00110ce8635819d17f0e3e387f1bd83e94dd
kuyltep <vladpetlu91@gmail.com>
2025-10-28 22:18:23 +0300

    update fastapi lifespan

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,16 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.services.kafka.kafka_service import kafka_service
 from src.services.kafka.config import kafka_settings
+from src.database import init_es_async_client
 
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+  
+  es_client = await init_es_async_client()
+  
+  print("Database connected succesfully")
   
   await kafka_service.start_producer()
   await kafka_service.start_consumer([kafka_settings.topic_in])
@@ -16,3 +21,4 @@ async def lifespan(app: FastAPI):
   yield
   
   await kafka_service.close()
+  await es_client.close()
