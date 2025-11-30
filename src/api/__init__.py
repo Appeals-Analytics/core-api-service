4f9481285ee7372df157ffa3241a927642811bd4
kuyltep <vladpetlu91@gmail.com>
2025-11-30 13:56:35 +0300

    update api file

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from api.files.router import files_router
-from api.batch_data.router import batch_router
+from src.api.files.router import files_router
+from src.api.batch_data.router import batch_router
+from src.api.dashboard.router import dashboard_router
+from src.api.message.router import messages_router
+from src.api.filters.router import filters_router
 
 main_router = APIRouter()
 
 main_router.include_router(files_router)
 main_router.include_router(batch_router)
+main_router.include_router(dashboard_router)
+main_router.include_router(messages_router)
+main_router.include_router(filters_router)
