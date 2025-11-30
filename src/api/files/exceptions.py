271a46fe57bc9cc132e569aded169db702ffe3d3
kuyltep <vladpetlu91@gmail.com>
2025-11-30 13:56:03 +0300

    update files api logic

--- src/api/files/exceptions.py
+++ src/api/files/exceptions.py
@@ -1,4 +1,4 @@
-from app.exceptions import BAD_REQUEST_EXCEPTION, CONTENT_TOO_LARGE_EXCEPTION
+from src.app.exceptions import BAD_REQUEST_EXCEPTION, CONTENT_TOO_LARGE_EXCEPTION
 
 INVALID_FILE_EXTENTION = BAD_REQUEST_EXCEPTION("Invalid file extention was provided")
 INVALID_FILE_SIZE = CONTENT_TOO_LARGE_EXCEPTION("Invalid file size was provided")
--- src/api/files/router.py
+++ src/api/files/router.py
@@ -1,19 +1,18 @@
 from fastapi import APIRouter, UploadFile
 
-from api.files.utils import validate_file, save_file
+from src.api.files.utils import validate_file, save_file
 
 files_router = APIRouter(prefix="/files", tags=["File"])
 
+
 @files_router.post("/upload")
 async def upload_file(file: UploadFile):
-  
-  validate_file(file=file)
-  
-  save_file(file=file)
-  
-  
+    validate_file(file=file)
+
+    save_file(file=file)
+
+
 @files_router.post("/multiple-upload")
 async def upload_files(files: list[UploadFile]):
-  
-  for file in files:
-    await upload_file(file=file)
+    for file in files:
+        await upload_file(file=file)
