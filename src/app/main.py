457a53411010d291e09d7758df8d120ea2dcafc3
kuyltep <vladpetlu91@gmail.com>
2025-12-20 00:05:12 +0300

    fix

--- src/app/main.py
+++ src/app/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 from src.api import main_router
 from src.app.utils import lifespan
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, root_path="/api")
 app.include_router(main_router)
